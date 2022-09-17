6190b45c244c533c76a254329ffb13fe36b57f10
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-09-17 01:12:47 +0100

    feat: Icon component

---
+++ components/Icon.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Icon = ({
+  className,
+  color = "inherit",
+  name = "arrow-right",
+  size = 16,
+}) => {
+  const [Icon, setIcon] = React.useState("");
+
+  React.useEffect(() => {
+    fetch(`/assets/svgs/${name}.svg`)
+      .then((res) => res.text())
+      .then((res) => {
+        if (res.startsWith("<svg")) return setIcon(res);
+        console.error(
+          `Icon: "${name}.svg" not found in ${process.env.PUBLIC_URL}/icons`
+        );
+        return setIcon("");
+      });
+  }, [name]);
+
+  if (!Icon) return null;
+
+  return (
+    <span
+      className={`icon ${className}`}
+      style={{ width: !size ? "100%" : size + "px", height: "100%", color }}
+      dangerouslySetInnerHTML={{ __html: Icon }}
+    />
+  );
+};
+
+Icon.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.string,
+  name: PropTypes.string,
+  size: PropTypes.number,
+};
+
+export default Icon;
