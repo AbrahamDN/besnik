dbe10db6299d723d4d903e2815ac418b139d02ff
Zay <Izpopsonic1@gmail.com>
2022-09-18 12:39:23 -0400

    refactor: Button - remove unused imports

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import buttonStyle from "./Button.module.scss";
-import styles from "../../styles/Home.module.scss";
 import Icon from "../Icon";
 const Button = ({ text, btnType, iconType }) => {
   return (
