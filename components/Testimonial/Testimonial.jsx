d4b26a1a430a92945f7a14a3157d159f705d72de
Jose Luis Garza <josegarzaramos@gmail.com>
2022-09-19 19:25:28 -0500

    feat: Testimonial component

---
+++ components/Testimonial/Testimonial.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import styles from "./Testimonial.module.scss";
+import Icon from "../Icon";
+
+const Testimonial = ({ message, name, position, company }) => {
+  return (
+    <div className={styles.testimonial__container}>
+      <div className={styles.testimonial__message__container}>
+        <Icon name="testimonial-brand" color="#fff" size="48" />
+
+        <p className={styles.testimonial__message}>{message}</p>
+
+        <div className={styles.testimonial__author}>
+          <div className={styles.testimonial__author__container}>
+            <span className={styles.testimonial__author__name}>{name}</span>
+            <span className={styles.testimonial_author_job}>{`${position}, ${company}`}</span>
+          </div>
+
+          <div className={styles.testimonial__controls}>
+            <button className={styles.testimonial__button}>
+              <Icon name={"chevron-left"} />
+            </button>
+            <button className={styles.testimonial__button}>
+              <Icon name={"chevron-right"} />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.testimonial__image}>
+        <Image src="/assets/testimonial-avatar.png" alt="testimonial picture" width="345" height="476" />
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import Gradient from "../components/Gradient/Gradient";
 import InsightBanner from "../components/InsightBanner/InsightBanner";
 import Footer from "../components/Footer/Footer";
 import FeatureCard from "../components/FeatureCard/FeatureCard";
+import Testimonial from "../components/Testimonial/Testimonial";
 
 export default function Home() {
   const insights = [
@@ -21,10 +22,7 @@ export default function Home() {
     <div className={styles.page}>
       <Head>
         <title>Besnik</title>
-        <meta
-          name="description"
-          content="Automate the way you search through hashtags and suggested profiles to find results 100x faster."
-        />
+        <meta name="description" content="Automate the way you search through hashtags and suggested profiles to find results 100x faster." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -34,78 +32,39 @@ export default function Home() {
         <Container size="xs">
           <section className={styles.feature__section}>
             <h2>Solutions for every need</h2>
-            <p>
-              Join thousands of marketers and entrepreneurs for a 2-day event at
-              the forefront of social commerce.
-            </p>
+            <p>Join thousands of marketers and entrepreneurs for a 2-day event at the forefront of social commerce.</p>
 
-            <FeatureCard
-              tag="Influencer"
-              title="Influencer marketing"
-              description="Create infinite pieces of content with our influencer product. Build,streamline,manage large-scale influencer programs"
-              image="/assets/feature-card-1.png"
-            />
+            <FeatureCard tag="Influencer" title="Influencer marketing" description="Create infinite pieces of content with our influencer product. Build,streamline,manage large-scale influencer programs" image="/assets/feature-card-1.png" />
 
-            <FeatureCard
-              tag="Community"
-              title="Community marketing"
-              description="Bring your relationships to new heights with our community product. Discover your biggest enthusiasts "
-              image="/assets/feature-card-2.png"
-              rowReverse
-            />
+            <FeatureCard tag="Community" title="Community marketing" description="Bring your relationships to new heights with our community product. Discover your biggest enthusiasts " image="/assets/feature-card-2.png" rowReverse />
 
-            <FeatureCard
-              tag="services"
-              title="Managed services"
-              description="Our full-service solution lets our experts do the heavy lifting to optimize your community for brand building and storytelling"
-              image="/assets/feature-card-3.png"
-            />
+            <FeatureCard tag="services" title="Managed services" description="Our full-service solution lets our experts do the heavy lifting to optimize your community for brand building and storytelling" image="/assets/feature-card-3.png" />
           </section>
         </Container>
 
-        <InsightBanner
-          title="Search through more than 11 million social media profiles"
-          desc="Heepsy’s search filters help you find exactly what you’re looking for, and our influencer reports provide you with the metrics"
-          insights={insights}
-        />
+        <InsightBanner title="Search through more than 11 million social media profiles" desc="Heepsy’s search filters help you find exactly what you’re looking for, and our influencer reports provide you with the metrics" insights={insights} />
 
         <Container size="xs">
           <section className={styles.service__section}>
             <h2>Solutions for every need</h2>
 
-            <p>
-              Join thousands of marketers and entrepreneurs for a 2-day event at
-              the forefront of social commerce.
-            </p>
+            <p>Join thousands of marketers and entrepreneurs for a 2-day event at the forefront of social commerce.</p>
 
-            <div className={styles.service__cards}>
-              ----- Service cards here -----
-            </div>
-          </section>
-
-          <section className={styles.testimonial__section}>
-            <h2>Happy Clients</h2>
-
-            <p>
-              The Marketing Accountability Standards Board (MASB) endorses the
-              definitions, purposes, and constructs of classes
-            </p>
-
-            <div className={styles.testimonial__card}>
-              ----- testimonial card here -----
-            </div>
+            <div className={styles.service__cards}>----- Service cards here -----</div>
           </section>
         </Container>
 
         <Container style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
+          <section className={styles.testimonial__section}>
+            <h2>Happy Clients</h2>
+
+            <p>The Marketing Accountability Standards Board (MASB) endorses the definitions, purposes, and constructs of classes</p>
+            <div className={styles.testimonial__card}>
+              <Testimonial message="Not weekly or monthly like other sites out there. This ensures that we offer prospective homebuyers and investors with the freshest- hottest deals on the Internet." name="Rowhan Smith" position="CEO" company="Foreclosure" />
+            </div>
+          </section>
           <Card padding="0">
-            <Gradient
-              borderRadius="1.25em"
-              height="auto"
-              title="Subscribe our newsletter"
-              description="Join thousands of marketers and entrepreneurs for a 2-day event at the forefront of social commerce."
-              buttonText="Subscribe"
-            />
+            <Gradient borderRadius="1.25em" height="auto" title="Subscribe our newsletter" description="Join thousands of marketers and entrepreneurs for a 2-day event at the forefront of social commerce." buttonText="Subscribe" />
           </Card>
         </Container>
       </main>
