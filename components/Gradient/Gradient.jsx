32aa9f77281c2947e40e1a19cb85cd4e3c7c1428
Jose Luis Garza <josegarzaramos@gmail.com>
2022-09-19 19:29:39 -0500

    refactor: Gradient - padding increased

--- components/Gradient/Gradient.jsx
+++ components/Gradient/Gradient.jsx
@@ -1,23 +1,25 @@
-import Button from '../Button/Button';
-import styles from './Gradient.module.scss'
+import Button from "../Button/Button";
+import styles from "./Gradient.module.scss";
 
-const Gradient = ({children, background, borderRadius, height, title, description, buttonText}) => {
-  let isBackground = background ? styles["gradient--absolute"]: styles["gradient--relative"];
-  let autoHeight = height == "auto" ? styles["gradient--auto"]: null;
+const Gradient = ({ children, background, borderRadius, height, title, description, buttonText }) => {
+  let isBackground = background ? styles["gradient--absolute"] : styles["gradient--relative"];
+  let autoHeight = height == "auto" ? styles["gradient--auto"] : null;
   let gradient = `${styles.gradient} ${isBackground} ${autoHeight}`;
 
-  return (<div className={gradient} style={{borderRadius}}>
-    <span className={styles["gradient__corner"]}></span>
-    <span className={styles["gradient__yellow-circle"]}></span>
-    <span className={styles["gradient__orange-circle"]}></span>
+  return (
+    <div className={gradient} style={{ borderRadius }}>
+      <span className={styles["gradient__corner"]}></span>
+      <span className={styles["gradient__yellow-circle"]}></span>
+      <span className={styles["gradient__orange-circle"]}></span>
 
-    <div className={styles["gradient__content"]}>
-      <h1 className={styles["gradient__title"]}>{title}</h1>
-      <p className={styles["gradient__description"]}>{description}</p>
-      
-      {buttonText && <Button btnType="primary" text={buttonText} iconType={"arrow-right"} />}
+      <div className={styles["gradient__content"]}>
+        <h2 className={styles["gradient__title"]}>{title}</h2>
+        <p className={styles["gradient__description"]}>{description}</p>
+
+        {buttonText && <Button btnType="primary" text={buttonText} iconType={"arrow-right"} />}
+      </div>
     </div>
-  </div>)
-}
+  );
+};
 
 export default Gradient;
